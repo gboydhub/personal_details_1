e1ade125c70b2cf7c4b3ed0a9d52ea54550d1c6a
Gary Boyd <eponick@gmail.com>
2018-07-09 13:56:29 -0400

    end script added

--- app.rb
+++ app.rb
@@ -11,3 +11,8 @@ end
 get '/get_nums/:name/:age' do
     erb :nums, :locals => {:name => params[:name], :age => params[:age]}
 end
+
+get '/end/:name/:age/:num1/:num2/:num3' do
+    erb :end, :locals => {:name => params[:name], :age => params[:age],
+        :num1 => params[:num1], :num2 => params[:num2], :num3 => params[:num3]}
+end
