985f5d7fa35e5dc7d9ed8d83b740fac61912dacb
Gary Boyd <eponick@gmail.com>
2018-07-09 13:45:51 -0400

    added / to paths

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ get '/' do
     erb :intro
 end
 
-get 'get_age/:name' do
+get '/get_age/:name' do
     erb :name, :locals => {:name => params[:name]}
 end
 
-get 'get_nums/:name/:age' do
+get '/get_nums/:name/:age' do
     erb :age, :locals => {:name => params[:name], :age => params[:age]}
 end
