b42b9cbe402d74422e8423e0ffe7c8022e31f8aa
Gary Boyd <eponick@gmail.com>
2018-07-09 13:18:23 -0400

    added age form

--- app.rb
+++ app.rb
@@ -7,3 +7,7 @@ end
 get '/:name' do
     erb :intro, :locals => {:name => params[:name]}
 end
+
+get '/:name/:age' do
+    erb :intro, :locals => {:name => params[:name], :age => params[:age]}
+end
--- public/js/intro.js
+++ public/js/intro.js
@@ -10,3 +10,17 @@ function postName()
         window.location = "/" + uname;
     }
 }
+
+function postAge()
+{
+    var uname = document.getElementById("username").value;
+    var uage = document.getElementById("age").value;
+    if(isNaN(parseint(uage)) && uage < 0)
+    {
+        document.getElementById("error_out").innerHTML = "Please input a valid age";
+    }
+    else
+    {
+        window.location = "/" + uname + "/" + uage;
+    }
+}
