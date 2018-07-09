81f1568870c00784c18a1e8a4000d6b9d16eb90c
Gary Boyd <eponick@gmail.com>
2018-07-09 13:32:48 -0400

    added 3 numbers js function

--- public/js/intro.js
+++ public/js/intro.js
@@ -24,3 +24,16 @@ function postAge()
         window.location = "/" + uname + "/" + uage;
     }
 }
+
+function postNums()
+{
+    var uname = document.getElementById("username").value;
+    var uage = document.getElementById("age").value;
+
+    var numA = document.getElementById("num_1").value;
+    var numB = document.getElementById("num_2").value;
+    var numC = document.getElementById("num_3").value;
+
+    window.location = "/" + uname + "/" + uage +
+            "/" + numA + "/" + numB + "/" + numC;
+}
